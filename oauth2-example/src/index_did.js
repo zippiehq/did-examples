cbb51e9272d4f9d44583f0c0a7140cedaa4369a2
davidbzip <david@zippie.com>
2023-03-10 13:26:12 +0100

    Updated the DID example so that it works now. Missing login

--- oauth2-example/src/index_did.js
+++ oauth2-example/src/index_did.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App_didTest';
 import reportWebVitals from './reportWebVitals';
+import { PermissionDesc, PlatformAgentUri} from '@zippie/did-core'
+import { PlatformProvider } from '@zippie/did-react-components'
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
 /*index.js*/
+const REQUESTED_PERMISSIONS = [PermissionDesc.READ_FULL_NAME, PermissionDesc.READ_EMAIL]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <GoogleOAuthProvider clientId="31266xxxxxxxxxxxxxxxxxx.apps.googleusercontent.com">
-            <App />
-    </GoogleOAuthProvider>
-);
 
+<PlatformProvider
+    clientId="ExampleApp"
+    agentUri={PlatformAgentUri.sandbox}
+    permissions={REQUESTED_PERMISSIONS}
+    config={{}}>
+    <App />
+  </PlatformProvider>
+);
 
 
 // If you want to start measuring performance in your app, pass a function
