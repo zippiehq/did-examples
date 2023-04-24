291553e60fc728874ef01e44bd02b289648b6ead
zippiehq <infra+did@zippie.com>
2023-04-24 11:35:04 +0000

    Update from https://github.com/zippiehq/did/commit/10c20012a482701a39d41d4ab37a7693774dcde0

--- oauth2/src/App.js
+++ oauth2/src/App.js
@@ -1,51 +1,83 @@
-import './App.css'
-import React, { useState } from 'react'
-import { googleLogout, useGoogleLogin } from '@react-oauth/google'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  const [profile, setProfile] = useState({})
+import { IAppData, PlatformError } from '@zippie/did-core'
 
-  const login = useGoogleLogin({
-    onSuccess: async (tokenResponse) => {
-      await axios
-        .get('https://www.googleapis.com/oauth2/v3/userinfo', {
-          headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
-        })
-        .then((res) => {
-          setProfile(res.data)
-        })
-        .catch((err) => console.log(err))
-    },
-    onError: (error) => console.log('Login Failed:', error),
-  })
+import { SignInForm, SignUpForm, usePlatform } from '@zippie/did-react-components'
 
-  // log out function to log the user out of google and set the profile array to null
-  const logOut = () => {
-    googleLogout()
-    setProfile({})
+//
+// Component which handles sign-up, sign-in, recovery, etc.
+//
+const SignInPage: React.FC = (setPubKey, setUserDetails, platform) => {
+  // this sets the states for bot sign in and  sign up
+  const [showSignUp, setShowSignUp] = useState(false)
+
+  // After sign in is complete and successful user profile is set
+  const onSignInComplete = async (result: IAppData | PlatformError) => {
+    console.info("I've been called")
+    const response = await platform?.getDerivedKeyInfo('m/0/1')
+    setPubKey(response)
+    const appData = await platform?.getApplicationData()
+    setUserDetails(appData.userDetails)
+    console.log(appData.userDetails)
   }
 
+  // After sign up is complete and successful user profile is set
+  const onSignUpComplete = (result: IAppData | PlatformError) => {
+    console.info("I've been called too")
+    console.info('sign-up-result:', result)
+  }
+
+  const onSignInButtonClick = () => setShowSignUp(false)
+  const onSignUpClick = () => {
+    setShowSignUp(true)
+    console.log('button clicked')
+  }
+
+  if (showSignUp) return <SignUpForm termsLink="" {...{ onSignInButtonClick, onSignUpComplete }} />
+  return <SignInForm {...{ onSignInComplete, onSignUpClick }} />
+}
+
+//
+//   The default export of this file wraps this component in a PlatformProvider, which allows
+// us to use the "usePlatform()" hook to start interacting with a users identity through the
+// platform APIs.
+//
+const App = () => {
+  const { isReady, isAppSignedIn, isUserSignedIn, platform } = usePlatform()
+  // declares and empty state for the user info
+  const [pubKey, setPubKey] = useState({})
+  const [userDetails, setUserDetails] = useState({})
+
+  // here when the user is signed in , user info is set
+  /* useEffect(() => {
+    if (!isAppSignedIn) return
+    const fetchInfo = async () => {
+      const response = await platform?.getDerivedKeyInfo('m/0/1')
+      setPubKey(response)
+      const appData = await platform?.getApplicationData()
+      setUserDetails(appData.userDetails)
+    }
+    fetchInfo().catch(console.error)
+  }, [isAppSignedIn, platform])
+*/
+  if (!isReady) return <h4>Loading...</h4>
+  if (!isAppSignedIn) return <SignInPage />
+
+  // This is how the user info is displayed after DID sign in/up is complete
   return (
     <div>
-      <h2>React Google Login</h2>
-      <br />
-      <br />
-      {Object.keys(profile).length ? (
-        <div>
-          <img src={profile.picture} alt="user" />
-          <h3>User Logged in</h3>
-          <p>Name: {profile.name}</p>
-          <p>Email Address: {profile.email}</p>
-          <p>User ID: {profile.sub}</p>
-          <br />
-          <br />
-          <button onClick={logOut}>Log out</button>
-        </div>
+      {isAppSignedIn ? (
+        <>
+          <h3>First Name: {userDetails.firstName}</h3>
+          <h3>Last Name: {userDetails.lastName}</h3>
+          <h3>Email: {userDetails.email}</h3>
+          <h3>Public Key: {pubKey.publicKey}</h3>
+        </>
       ) : (
-        <button onClick={() => login()}>Sign in with Google 🚀 </button>
+        'Loading'
       )}
     </div>
   )
 }
+
 export default App
